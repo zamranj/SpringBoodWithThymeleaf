128cff142af526ef5e0dc5ffbe082cec7ce928bd
Zamran J <family_zj@yahoo.com>
2021-10-13 22:40:03 -0500

    thymeleaf demo stuff

--- src/main/java/com/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/com/launchcode/codingevents/controllers/EventController.java
@@ -1,7 +1,33 @@
 package com.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("events")
 public class EventController {
+    private static List<String> events = new ArrayList<>();
+    @GetMapping
+    public String displayEvents(Model model){
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+    //located at events/create
+    @GetMapping("create")
+    public String displayCreateEventForm(){
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+    }
 }
